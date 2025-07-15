ba9c3f61173f51f98fa4e325b7e9e04c54ecfe13
joe <suprefuner@gmail.com>
2025-07-15 15:56:35 +0800

    01: setup

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-red-500 text-3xl">
+        Hello World
+      </h1>
+
+      <Button>
+        click me
+      </Button>
+    </div>
+  );
+}
